c76704e202a45db68eaa10686be7b723f4fd4528
abromley <abromley@geometricresults.co.uk>
2024-03-05 13:16:04 +0000

    Refactor tests to include more test cases

--- checkout_test.go
+++ checkout_test.go
@@ -6,20 +6,22 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-
 func TestGetCheckoutReturnsEpmtyCheckoutStruct(t *testing.T) {
-	c := GetCheckout()	
+	c := GetCheckout()
 	assert.Len(t, c.Basket, 0)
 }
 
 func TestScanIncreasesBasketCounts(t *testing.T) {
-	products := []string{ "A", "B", "C", "A"}
+	products := []string{"A", "B", "C", "A"}
 	c := setupCheckout(products)
 
 	assert.Equal(t, 2, c.Basket["A"]) // checking order that these are input does not cause issues
 	assert.Equal(t, 1, c.Basket["B"])
 	assert.Equal(t, 1, c.Basket["C"])
 	assert.Equal(t, 0, c.Basket["D"])
+
+	c.scan("A")
+	assert.Equal(t, 3, c.Basket["A"])
 }
 
 func TestScanErrorsForUnkownProduct(t *testing.T) {
@@ -28,12 +30,35 @@ func TestScanErrorsForUnkownProduct(t *testing.T) {
 	assert.Error(t, err)
 }
 
-func TestGetTotalPriceBasic(t *testing.T) {
-	c := setupCheckout([]string{"A", "B", "C", "C", "A", "D"})
-	
-	price := c.getTotalPrice()
-	expected := 50 + 30 + 20 + 20 + 50 + 15
-	assert.Equal(t, expected, price)
+func TestGetTotalPrice(t *testing.T) {
+	tests := map[string]struct {
+		input    []string
+		expected int
+	}{
+		"basic - one product": {
+			input: []string{"D"},
+			expected: 15,
+		},
+		"basic - multiple of 1": {
+			input: []string{"C", "C", "C", "C"},
+			expected: 4 * 20,
+		},
+		"basic - mixture": {
+			input:    []string{"A", "B", "C", "C", "A", "D"},
+			expected: 50 + 30 + 20 + 20 + 50 + 15,
+		},
+		"no products scanned": {
+			input:    []string{},
+			expected: 0,
+		},
+	}
+
+	for _, test := range tests {
+		c := setupCheckout(test.input)
+
+		price := c.getTotalPrice()
+		assert.Equal(t, test.expected, price)
+	}
 }
 
 func setupCheckout(SKUs []string) checkout {
@@ -43,4 +68,3 @@ func setupCheckout(SKUs []string) checkout {
 	}
 	return c
 }
-
